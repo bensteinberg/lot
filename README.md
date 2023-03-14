964b352c7e482c87f2ddea6a949116f524d6fada
Ben Steinberg <ben@potswift.org>
2023-03-14 13:28:18 -0400

    Look in all profiles

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Sometimes I want to get a list of open browser tabs, particularly from
 a remote machine. This script does so; it does not require the browser
 to be running, as it looks in the session backup.
 
-At the moment, it works for Firefox only, and only looks at the first
-profile it finds.
+At the moment, it works for Firefox only.
 
 To install, run `pip install git+https://github.com/bensteinberg/lot.git`.
--- lot/list_open_tabs.py
+++ lot/list_open_tabs.py
@@ -4,29 +4,31 @@ from pathlib import Path
 
 """
 This program lists your open Firefox tabs. It does not require Firefox
-to be running, as it looks in the session backup.
+to be running, as it looks in the session backup. It looks in all of your
+profiles.
 """
 
 def cli():
     # the recovery file is in the Firefox profile, the location of which can
     # vary with OS and installation method
-    # this will not deal well with multiple profiles
+    # look at all profiles
     # if this rglob took too long, we could guess at the profile location
-    recovery = list(Path.home().rglob('sessionstore-backups/recovery.js*'))[0]
+    recoveries = list(Path.home().rglob('sessionstore-backups/recovery.js*'))
 
     # method from https://unix.stackexchange.com/a/389360
     # and suffix nuance from https://unix.stackexchange.com/a/385026
-    with open(recovery, "rb") as f:
-        if recovery.suffix == '.jsonlz4':
-            _ = f.read(8)  # magic
-            data = json.loads(
-                lz4.block.decompress(f.read()).decode("utf-8")
-            )
-        else:
-            data = json.load(f)
+    for recovery in recoveries:
+        with open(recovery, "rb") as f:
+            if recovery.suffix == '.jsonlz4':
+                _ = f.read(8)  # magic
+                data = json.loads(
+                    lz4.block.decompress(f.read()).decode("utf-8")
+                )
+            else:
+                data = json.load(f)
 
-        for window in data["windows"]:
-            for tab in window["tabs"]:
-                i = int(tab["index"]) - 1
-                url = tab["entries"][i]["url"]
-                print(url)
+            for window in data["windows"]:
+                for tab in window["tabs"]:
+                    i = int(tab["index"]) - 1
+                    url = tab["entries"][i]["url"]
+                    print(url)
